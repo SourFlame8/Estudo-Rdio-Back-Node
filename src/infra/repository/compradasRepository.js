969f72bc85a94f1edb3982dadd44e6696092bd6e
sourflame8 <paulohh77@hotmail.com>
2022-11-29 11:00:11 -0300

    compradas e pendentes repository feitas

--- src/infra/repository/compradasRepository.js
+++ src/infra/repository/compradasRepository.js
@@ -5,6 +5,36 @@ class compradasRepository {
     constructor() {
         this.db = db;
     }
+
+    listAll() {
+        return new Promise((resolve, reject) => {
+            try {
+                this.db.query(`SELECT * FROM compradas`,
+                    [],
+                    async (error, response) => {
+                        if (error) return reject(new Error(error));
+                        return resolve({ comrpadas: response });
+                    })
+            } catch (error) {
+                return reject(new Error(error));
+            }
+        })
+    }
+
+    registrer(data) {
+        return new Promise((resolve, reject) => {
+            try {
+                this.db.query(`INSERT INTO compradas (nome, artista, album, licenca_comprada, venc_licenca, gravadora) VALUES (?,?,?,?,?,?)`,
+                    [data.nome, data.artista, data.album, data.licencaComprada, data.vencLicenca, data.gravadora],
+                    async (error, response) => {
+                        if (error) return reject(new Error(error));
+                        return resolve({ sucess: "Musica registrada com sucesso" })
+                    })
+            } catch (error) {
+                return reject(new Error(error));
+            }
+        })
+    }
 }
 
 module.exports = compradasRepository;
--- src/infra/repository/pendentesRepository.js
+++ src/infra/repository/pendentesRepository.js
@@ -5,6 +5,68 @@ class pendentesRepository {
     constructor() {
         this.db = db;
     }
+
+    /**
+     * Lista todas as musicas pendentes
+     * @returns Array com dados das musicas pendentes
+     */
+
+    listAll() {
+        return new Promise((resolve, reject) => {
+            try {
+                this.db.query(`SELECT * FROM pendentes`,
+                    [],
+                    async (error, response) => {
+                        if (error) return reject(new Error(error));
+                        return resolve({ pendentes: response });
+                    })
+            } catch (error) {
+                return reject(new Error(error))
+            }
+        })
+    }
+
+    /**
+     * Registra uma musica pendente
+     * @param {*} data dados da musica
+     * @returns Musica registrada com sucesso
+     */
+
+    register(data) {
+        return new Promise((resolve, reject) => {
+            try {
+                this.db.query(`INSERT INTO pendentes (nome, artista, album, gravadora, situacao_compra) VALUES (?,?,?,?,?)`,
+                    [data.nome, data.artista, data.album, data.gravadora, data.situacaoCompra],
+                    async (error, response) => {
+                        if (error) return reject(new Error(error));
+                        return resolve({ sucess: "Musica registrada com sucesso" });
+                    })
+            } catch (error) {
+                return reject(new Error(error));
+            }
+        })
+    }
+
+    /**
+     * Remove uma musica pendente
+     * @param {*} id id da musica
+     * @returns Musica removida com sucesso
+     */
+
+    remove(id){
+        return new Promise((resolve, reject) => {
+            try{
+                this.db.query(`REMOVE FROM pendentes WHERE id = ?`, 
+                [id],
+                async (error, response) => {
+                    if (error) return reject (new Error(error));
+                    return resolve({sucess: "Musica removida com sucesso"})
+                })
+            } catch (error) {
+                return reject (new Error(error));
+            }
+        })
+    }
 }
 
 module.exports = pendentesRepository;
